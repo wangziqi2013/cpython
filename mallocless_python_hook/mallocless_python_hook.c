63b037d8259db16442eb58f3846d19aba88f22c8
Ziqi Wang <675973885@qq.com>
2021-12-18 05:53:16 -0500

    Updating mallocless hook

--- mallocless_python_hook/mallocless_python_hook.c
+++ mallocless_python_hook/mallocless_python_hook.c
@@ -8,24 +8,28 @@ extern "C" {
 void mallocless_python_hook_PyMem_RawMalloc(uint64_t size, void *ptr) {
   //(void)size; (void)ptr;
   zsim_magic_op_malloc((int)size, (uint64_t)ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
 void mallocless_python_hook_PyMem_RawCalloc(uint64_t count, uint64_t size, void *ptr) {
   //(void)count; (void)size; (void)ptr;
   zsim_magic_op_calloc((int)count, (int)size, (uint64_t)ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
 void mallocless_python_hook_PyMem_RawRealloc(void *old_ptr, uint64_t size, void *new_ptr) {
   //(void)old_ptr; (void)size; (void)new_ptr;
   zsim_magic_op_realloc((uint64_t)old_ptr, (int)size, (uint64_t)new_ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
 void mallocless_python_hook_PyMem_RawFree(void *ptr) {
   //(void)ptr;
   zsim_magic_op_free((uint64_t)ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
--- mallocless_python_hook/zsim_magic_ops.h
+++ mallocless_python_hook/zsim_magic_ops.h
@@ -4,12 +4,14 @@
 
 #include <stdint.h>
 
-//* zsim - zsim related functions, called by application to communicate with the simulator
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 enum {
   ZSIM_MAGIC_OP_HELLO_WORLD,
   ZSIM_MAGIC_OP_PRINT_STR,
-  ZSIM_START_SIM,
+  ZSIM_MAGIC_OP_START_SIM,
   ZSIM_MAGIC_OP_GET_ADDR_MAPPING,
   // Random number related
   ZSIM_MAGIC_OP_GET_RAND32,
@@ -19,6 +21,9 @@ enum {
   ZSIM_MAGIC_OP_CALLOC,
   ZSIM_MAGIC_OP_REALLOC,
   ZSIM_MAGIC_OP_FREE,
+  // Progress control
+  ZSIM_MAGIC_OP_PAUSE_SIM,     // Pause simulation after it has started
+  ZSIM_MAGIC_OP_RESUME_SIM,    // Resume simulation
 };
 
 // The struct that contains full information about the magic op
@@ -44,68 +49,17 @@ typedef struct {
   uint64_t old_ptr;   // realloc uses this
 } zsim_alloc_t;
 
-// General-purpose magic op interface
-inline static void zsim_magic_op(zsim_magic_op_t *op) {
-  // Write op pointer into R10
-  __asm__ __volatile__("mov %0, %%r10\n\t"
-                     : /* no output */
-                     : "a"(op)
-                     : "%r10");
-  // XOR R10, R10
-  __asm__ __volatile__ (".byte 0x4D, 0x87, 0xD2");
-  return;
-}
+void zsim_magic_op(zsim_magic_op_t *op);
+void zsim_magic_op_hello_world();
+void zsim_magic_op_print_str(const char *s);
+void zsim_magic_op_start_sim();
+void zsim_magic_op_pause_sim();
+void zsim_magic_op_resume_sim();
+void zsim_magic_op_malloc(int size, uint64_t ptr);
+void zsim_magic_op_calloc(int count, int size, uint64_t ptr);
+void zsim_magic_op_realloc(uint64_t old_ptr, int size, uint64_t new_ptr);
+void zsim_magic_op_free(uint64_t ptr);
 
-// Short hands that just call into the general interface
-
-void zsim_magic_op_hello_world() {
-  zsim_magic_op_t op;
-  op.op = ZSIM_MAGIC_OP_HELLO_WORLD;
-  zsim_magic_op(&op);
-  return;
+#ifdef __cplusplus
 }
-
-void zsim_magic_op_malloc(int size, uint64_t ptr) {
-  zsim_magic_op_t op;
-  zsim_alloc_t alloc;
-  alloc.size = size;
-  alloc.ptr = ptr;
-  op.arg = &alloc;
-  op.op = ZSIM_MAGIC_OP_MALLOC;
-  zsim_magic_op(&op);
-  return;
-}
-
-void zsim_magic_op_calloc(int count, int size, uint64_t ptr) {
-  zsim_magic_op_t op;
-  zsim_alloc_t alloc;
-  alloc.count = count;
-  alloc.size = size;
-  alloc.ptr = ptr;
-  op.arg = &alloc;
-  op.op = ZSIM_MAGIC_OP_CALLOC;
-  zsim_magic_op(&op);
-  return;
-}
-
-void zsim_magic_op_realloc(uint64_t old_ptr, int size, uint64_t new_ptr) {
-  zsim_magic_op_t op;
-  zsim_alloc_t alloc;
-  alloc.old_ptr = old_ptr;
-  alloc.size = size;
-  alloc.new_ptr = new_ptr;
-  op.arg = &alloc;
-  op.op = ZSIM_MAGIC_OP_REALLOC;
-  zsim_magic_op(&op);
-  return;
-}
-
-void zsim_magic_op_free(uint64_t ptr) {
-  zsim_magic_op_t op;
-  zsim_alloc_t alloc;
-  alloc.ptr = ptr;
-  op.arg = &alloc;
-  op.op = ZSIM_MAGIC_OP_FREE;
-  zsim_magic_op(&op);
-  return;
-}
+#endif
