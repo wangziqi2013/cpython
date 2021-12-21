cf366ce51bc3bc44d2feca250f1829dcd8820ae6
Ziqi Wang <675973885@qq.com>
2021-12-21 01:27:05 -0500

    Adding magic op to obj malloc

--- mallocless_python_hook/mallocless_python_hook.c
+++ mallocless_python_hook/mallocless_python_hook.c
@@ -34,22 +34,26 @@ void mallocless_python_hook_PyMem_RawFree(void *ptr) {
 }
 
 void mallocless_python_hook_PyObject_Malloc(uint64_t size, void *ptr) {
-  (void)size; (void)ptr;
+  zsim_magic_op_malloc((int)size, (uint64_t)ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
 void mallocless_python_hook_PyObject_Calloc(uint64_t count, uint64_t size, void *ptr) {
-  (void)count; (void)size; (void)ptr;
+  zsim_magic_op_calloc((int)count, (int)size, (uint64_t)ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
 void mallocless_python_hook_PyObject_Realloc(void *old_ptr, uint64_t size, void *new_ptr) {
-  (void)old_ptr; (void)size; (void)new_ptr;
+  zsim_magic_op_realloc((uint64_t)old_ptr, (int)size, (uint64_t)new_ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
 void mallocless_python_hook_PyObject_Free(void *ptr) {
-  (void)ptr;
+  zsim_magic_op_free((uint64_t)ptr);
+  zsim_magic_op_resume_sim();
   return;
 }
 
