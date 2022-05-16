f6f65179d4d1bfc7e77f50539eaab64613b4e724
Ziqi <675973885@qq.com>
2022-05-16 09:25:43 -0400

    Adding end call back

--- Programs/python.c
+++ Programs/python.c
@@ -3,6 +3,8 @@
 #include "Python.h"
 #include "pycore_pylifecycle.h"
 
+#include "../mallocless_python_hook/mallocless_python_hook.h"
+
 #ifdef MS_WINDOWS
 int
 wmain(int argc, wchar_t **argv)
@@ -13,6 +15,8 @@ wmain(int argc, wchar_t **argv)
 int
 main(int argc, char **argv)
 {
-    return Py_BytesMain(argc, argv);
+    int ret = Py_BytesMain(argc, argv);
+    zsim_magic_op_end_sim();
+    return ret;
 }
 #endif
